657ad0402beea2ebda2c30ef69ce5df126b0f36f
33sandyy <33sandyp@gmail.com>
2025-09-19 16:19:14 +0530

    model training and evaluation completed!

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -17,7 +18,7 @@ if __name__=="__main__":
         logging.info("Initiate data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data ingestion completed!")
-        print(dataingestionartifact)
+        # print(dataingestionartifact)
 
 
         #data validation
@@ -26,7 +27,7 @@ if __name__=="__main__":
         logging.info("Initiate the data validation")
         data_validation_artfact=data_validation.initiate_data_validation()
         logging.info("Data validation completed")
-        print(data_validation_artfact)
+        # print(data_validation_artfact)
 
         #data transformation
         data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
@@ -35,7 +36,15 @@ if __name__=="__main__":
         logging.info("Initiate the data transformation")
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logging.info("Data transformation completed!")
-        print(data_transformation_artifact)
+        # print(data_transformation_artifact)
+
+        #model trainer
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config=trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config,data_transformation_artifact)
+
+        logging.info("Initiate Model Traner")
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Traner Completed!")
 
 
     except Exception as e:

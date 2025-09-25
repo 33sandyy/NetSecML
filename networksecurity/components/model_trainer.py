f25aea8f5e25f5e5b443a03b050241f0bda3a863
33sandyy <33sandyp@gmail.com>
2025-09-25 18:29:24 +0530

    dagshun integration and saving final model

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,6 +22,11 @@ from sklearn.ensemble import (
 )
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='33sandyy', repo_name='NetSecML', mlflow=True)
+
+
+
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,
                  data_transformation_artifact:DataTransformationArtifact):
@@ -103,6 +108,7 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=Network_Model)
+        save_object("final_model/model.pkl",best_model)
 
         #Model Trainer Artifact
         model_trainer_artifact=ModelTrainerArtifact(
@@ -132,7 +138,7 @@ class ModelTrainer:
                 test_arr[:,-1],
             )
 
-            model_trainer_artifact=self.train_model(x_train,y_train,x_test,y_test)
+            model_trainer_artifact=self.train_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test)
 
             return model_trainer_artifact
 
